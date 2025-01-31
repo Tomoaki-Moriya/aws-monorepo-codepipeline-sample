427f3fed7ca4ee7f695c2686d89a2712fbf61b6f
Tomoaki-Moriya <tomoaki.moriya1096@gmail.com>
2025-02-01 06:40:23 +0900

    modified

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -2,9 +2,9 @@
 import * as cdk from "aws-cdk-lib";
 import { LambdaFooPipelineStack } from "../lib/lambda-foo-pipeline-stack";
 import { ArtifactBucketStack } from "../lib/artifact-bucket-stack";
-import { LambdaFooBarPipelineStack } from "../lib/lambda-foobar-pipeline-stack";
+import { LambdaFoobarPipelineStack } from "../lib/lambda-foobar-pipeline-stack";
 
 const app = new cdk.App();
 new ArtifactBucketStack(app, "ArtifactBucketStack", {});
 new LambdaFooPipelineStack(app, "LambdaFooPipelineStack", {});
-new LambdaFooBarPipelineStack(app, "LambdaFooBarPipelineStack", {});
+new LambdaFoobarPipelineStack(app, "LambdaFoobarPipelineStack", {});
--- infrastructure/lib/lambda-foobar-pipeline-stack.ts
+++ infrastructure/lib/lambda-foobar-pipeline-stack.ts
@@ -3,11 +3,11 @@ import { Construct } from "constructs";
 import { MonorepoLambdaPipelineConstruct } from "./monorepo-lambda-pipeline-construct";
 import { ARTIFACT_BUCKET_NAME } from "./artifact-bucket-stack";
 
-export class LambdaFooBarPipelineStack extends cdk.Stack {
+export class LambdaFoobarPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new MonorepoLambdaPipelineConstruct(this, "LambdaFooBarPipeline", {
+    new MonorepoLambdaPipelineConstruct(this, "LambdaFoobarPipeline", {
       repo: "aws-monorepo-codepipeline-sample",
       branch: "main",
       projectName: "lambda-project-foobar",
